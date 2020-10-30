a2404f60dab5032800e6d362771e1ed2dafaeb4c
Kazuki Yoshimura <kazuki.727.44.1999@gmail.com>
2020-10-31 06:07:03 +0900

    Googleログイン実装

---
+++ lib/model/signin_model.dart
@@ -0,0 +1,53 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+String name;
+String imageURL;
+
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken:googleSignInAuthentication.idToken
+  );
+
+  final UserCredential authResult = await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null){
+
+    assert(user.displayName != null);
+    assert(user.photoURL != null);
+
+    if (name.contains(" ")){
+      name = name.substring(0, name.indexOf(" "));
+    }
+
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+
+    print('GoogleSignIn : $user');
+
+    return '$user';
+  }
+
+  return null;
+
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print('User sign out');
+}
